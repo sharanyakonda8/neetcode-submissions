c17f7f6500d8121e8fc14bfa7c7f0e286ed96c7c
sharanyakonda8 <sharanyakonda8@gmail.com>
2026-07-31 15:20:30 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       stack<char>st;
+       if(s.size()%2!=0)return false;
+       for(int i=0;i<s.size();i++){
+        if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+            st.push(s[i]);
+        }else{
+            if(st.empty())return false;
+            char t=st.top();
+            if((s[i]==')' && t=='(') || (s[i]==']' && t=='[') || (s[i]=='}' && t=='{')){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+
+        }
+       }
+       return st.empty(); 
+    }
+};
