b44072a5a6f9dae37d74ca23c41a249a7cab32e3
sharanyakonda8 <sharanyakonda8@gmail.com>
2026-08-11 15:55:39 +0530

    Add: pow-x-n - submission-6

---
+++ Data Structures & Algorithms/pow-x-n/submission-6.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+       double ans=1;
+       bool isn=0;
+       if(n==0)return 1;
+       if(n==1 || x==1)return x;
+       if(x==-1){
+        if(n%2==0)return 1;
+        else return -1;}
+       
+       if(n<0){
+        n=-n;
+        isn=1;
+       }
+       while(n){
+        ans=ans*x;
+        n--;
+       } 
+       if(isn)return 1/ans;
+       return ans;
+    }
+};
